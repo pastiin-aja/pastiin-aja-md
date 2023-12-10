d3b51bb85a8061d1d7bf58d3f6f564ce3456e5d4
Fayza Nadia <fayzanadia@gmail.com>
2023-12-10 15:53:05 +0700

    chore: folderization

--- app/src/main/java/com/bangkit/pastiinaja/ui/main/MainActivity.kt
+++ app/src/main/java/com/bangkit/pastiinaja/ui/main/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.bangkit.pastiinaja
+package com.bangkit.pastiinaja.ui.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bangkit.pastiinaja.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
