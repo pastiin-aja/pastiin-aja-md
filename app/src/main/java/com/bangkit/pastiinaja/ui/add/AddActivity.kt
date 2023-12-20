f099be4bc0e96224408df2971a69207b24ff5874
Fayza <fayzanadia@gmail.com>
2023-12-20 20:04:04 +0700

    feat: add main fragment

--- app/src/main/java/com/bangkit/pastiinaja/ui/add/AddActivity.kt
+++ app/src/main/java/com/bangkit/pastiinaja/ui/add/AddActivity.kt
@@ -1,12 +1,39 @@
 package com.bangkit.pastiinaja.ui.add
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowInsets
+import android.view.WindowManager
 import com.bangkit.pastiinaja.R
+import com.bangkit.pastiinaja.databinding.ActivityAddBinding
 
 class AddActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityAddBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_add)
+        binding = ActivityAddBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setupView()
+
+        val initialFragment = AddMainFragment()
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment_container, initialFragment)
+            .commit()
+    }
+
+    private fun setupView() {
+        @Suppress("DEPRECATION")
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.insetsController?.hide(WindowInsets.Type.statusBars())
+        } else {
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+            )
+        }
+        supportActionBar?.hide()
     }
 }
