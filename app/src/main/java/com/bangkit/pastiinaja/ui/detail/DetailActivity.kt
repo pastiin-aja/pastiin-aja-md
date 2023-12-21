48ed242a2fbd94929e461315728ac96a9439873c
Fayza <fayzanadia@gmail.com>
2023-12-21 18:14:06 +0700

    feat: detail layout

--- app/src/main/java/com/bangkit/pastiinaja/ui/detail/DetailActivity.kt
+++ app/src/main/java/com/bangkit/pastiinaja/ui/detail/DetailActivity.kt
@@ -1,10 +1,13 @@
-package com.bangkit.pastiinaja.detail
+package com.bangkit.pastiinaja.ui.detail
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.bangkit.pastiinaja.R
+import com.bangkit.pastiinaja.databinding.ActivityDetailBinding
 
 class DetailActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityDetailBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail)
