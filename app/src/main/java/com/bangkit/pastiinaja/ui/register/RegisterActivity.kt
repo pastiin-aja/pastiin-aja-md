ccd889a3f1cd33c85cddeb2b012c1fa97536e986
Fayza <fayzanadia@gmail.com>
2023-12-21 11:22:53 +0700

    refactor: login and register

---
+++ app/src/main/java/com/bangkit/pastiinaja/ui/register/RegisterActivity.kt
@@ -0,0 +1,12 @@
+package com.bangkit.pastiinaja.ui.register
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.bangkit.pastiinaja.R
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
---
+++ app/src/main/java/com/bangkit/pastiinaja/ui/welcome/WelcomeActivity.kt
@@ -0,0 +1,12 @@
+package com.bangkit.pastiinaja.ui.welcome
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.bangkit.pastiinaja.R
+
+class WelcomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_welcome)
+    }
+}
