c4c2d23c404572d287c33d9e0e23ef2b63a55aa8
rejamr <rezamuhammadramzi@gmail.com>
2023-12-21 13:12:26 +0700

    feat : text

---
+++ app/src/main/java/com/bangkit/pastiinaja/detail/DetailActivity.kt
@@ -0,0 +1,12 @@
+package com.bangkit.pastiinaja.detail
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.bangkit.pastiinaja.R
+
+class DetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail)
+    }
+}
